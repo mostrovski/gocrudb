1067c117bab8c178709fa61315a4c8420e8b78d2
mostrovski <ostrovskiime@gmail.com>
2026-01-10 14:44:28 +0100

    Adjust exceptions

--- exception/resource-not-found.go
+++ exception/resource-not-found.go
@@ -3,15 +3,9 @@ package exception
 import "fmt"
 
 type ResourceNotFound struct {
-	Resource string
-	Id       any
+	Id any
 }
 
 func (e ResourceNotFound) Error() string {
-	r := "Resource"
-	if e.Resource != "" {
-		r = e.Resource
-	}
-
-	return fmt.Sprintf("%s not found. ID: %v", r, e.Id)
+	return fmt.Sprintf("Resource not found. ID: %v", e.Id)
 }
